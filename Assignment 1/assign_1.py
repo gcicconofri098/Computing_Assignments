7d3f9fe2cdc267bc4f0467349532c881541debb9
Gloria <g.cicconofri@studenti.unipi.it>
2021-10-05 15:48:11 +0200

    added comments to the code

--- Assignment 1/assign_1.py
+++ Assignment 1/assign_1.py
@@ -1,33 +1,38 @@
 import matplotlib.pyplot as plt
 import time
 import string
-file= open ("pg1497.txt", encoding="utf8")
+
+#open the file, eliminate the formattation and make all the letter lower case
+file= open ("pg1497.txt", encoding="utf8") 
 text = file.read().replace("\n", " " )
 text = text.lower()
 
+#call the alphabet letters, ordered
 alphabet= (string.ascii_lowercase)
+#define the total number of letters and create a dictionary
 total = 0
-
 a={}
-b={}
+#fill the dictionary and update the total number of letters
 for x in (alphabet):
     a[x]=text.count(x)
     total= total+text.count(x)
     
-
+#print check
 print(a)
 print(len(a))
-b=a.copy()
-#print(b)
+
+#substitute the total number of a specific letter with the relative frequency
 for x in (alphabet):
-    b[x]=b[x]/total
+    a[x]=a[x]/total
 
-print(b)
+#print the relative frequencies
+for x, a[x] in a.items():
+    print('{}: {:.3f}%'.format(x, a[x] * 100.))
 
-for x, b[x] in b.items():
-    print('{}: {:.3f}%'.format(x, b[x] * 100.))
-
-plt.bar(b.keys(), b.values(), color= 'red')
-#plt.hist(b.values(), len(b.keys()), color= 'yellow')
+#create a histogram with the relative frequencies for every letter
+plt.bar(a.keys(), a.values(), color= 'red')
+#plt.hist(a.values(), len(a.keys()), color= 'yellow')
 plt.show()
+
+#print the elapsed time of the process
 print(time.process_time())
