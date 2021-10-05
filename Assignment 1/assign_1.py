3441f39abea6f53aec224dbde5352c76485bfb75
Gloria <g.cicconofri@studenti.unipi.it>
2021-10-05 12:27:46 +0200

    first commit

---
+++ Assignment 1/assign_1.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import string
+file= open ("pg1497.txt", encoding="utf8")
+text = file.read().replace("\n", " " )
+text = text.lower()
+
+alphabet= (string.ascii_lowercase)
+total = 0
+
+a={}
+b={}
+for x in (alphabet):
+    a[x]=text.count(x)
+    total= total+text.count(x)
+    
+
+print(a)
+print(len(a))
+b=a.copy()
+#print(b)
+for x in (alphabet):
+    b[x]=b[x]/total
+
+print(b)
+
+for x, b[x] in b.items():
+    print('{}: {:.3f}%'.format(x, b[x] * 100.))
+
+plt.bar(b.keys(), b.values(), color= 'red')
+#plt.hist(b.values(), len(b.keys()), color= 'yellow')
+plt.show()
